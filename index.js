fb2d1d27ef0ac6c1222c9a334d80f22466bc19a1
cathy <cathy.snailmail@gmail.com>
2023-03-11 17:20:09 -0800

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+
+let employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway St'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+        let newObject = {...employee};
+        
+        newObject[key] = value;
+
+        return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    let newObject = {...employee};
+    newObject[key] = undefined;
+
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key){
+    employee[key] = undefined;
+
+    return employee;
+}
+
